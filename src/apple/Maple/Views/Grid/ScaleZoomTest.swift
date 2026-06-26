9d1f7fa717f0afd6bbe25b3a597497351025ab5f
Zubair Lawrence <zubair@lawrence.io>
2026-06-26 01:53:01 -0400

    proto(grid): fade delay 0.4s -> 0.2s (#1550)

--- src/apple/Maple/Views/Grid/ScaleZoomTest.swift
+++ src/apple/Maple/Views/Grid/ScaleZoomTest.swift
@@ -212,7 +212,7 @@ struct ScaleZoomTest: View {
                     tScale = newScale
                 }
                 // Content dissolve: delayed start, then dissolve.
-                withAnimation(.smooth(duration: 0.45).delay(0.4)) {
+                withAnimation(.smooth(duration: 0.45).delay(0.2)) {
                     fade = 1
                 } completion: {
                     scale = newScale
