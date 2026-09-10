b565af6a4a553fce403822d7960424824b279ff2
Zubair Lawrence <zubair@lawrence.io>
2026-09-10 01:54:51 -0400

    fix(maple): revert dist artifacts from git diff and use fileURLToPath in packaging scripts

--- src/maple/dist/index.d.ts
+++ src/maple/dist/index.d.ts
@@ -2,7 +2,6 @@
  * Maple - The official image processing package for Maple.
  */
 export * from './types';
-export * from './platform';
 export * from './native';
 export * from './export';
 export * from './builder';
